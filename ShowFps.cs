d90f4ab72d5409a1e2d7aef37a74242fcc275741
Shlok gupta <shlokcoder12@gmail.com>
2022-01-24 12:33:57 +0530

    Add files via upload

---
+++ ShowFps.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowFps : MonoBehaviour
+{
+    private float pollingTime = 1f;
+	private float time;
+	private int frameCount;
+    public Text m_Text;
+
+    void Update() {
+		// Update time.
+		time += Time.deltaTime;
+
+		// Count this frame.
+		frameCount++;
+
+		if (time >= pollingTime) {
+			// Update frame rate.
+			int frameRate = Mathf.RoundToInt((float)frameCount / time);
+            m_Text.text = "Game FPS :";
+			m_Text.text = frameRate.ToString() + " FPS";
+
+			// Reset time and frame count.
+			time -= pollingTime;
+			frameCount = 0;
+		}
+	}
+}
+
+
+	
+	
+
+ 
+	
